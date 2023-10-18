b3d5442b9d7af6dbb26ee4c38c29a72b53f29f81
wallacemod <wallacepatos@hotmail.com>
2023-10-18 20:51:15 -0300

    Optimized `MainActivity` imports

--- app/src/main/java/com/wallacemod/android_pokedex/view/MainActivity.kt
+++ app/src/main/java/com/wallacemod/android_pokedex/view/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.wallacemod.android_pokedex.view
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.wallacemod.android_pokedex.R
-import com.wallacemod.android_pokedex.model.api.ListPokemonApiResult
-import com.wallacemod.android_pokedex.model.api.PokeApiService
-import com.wallacemod.android_pokedex.view.list.PokemonItem
 import com.wallacemod.android_pokedex.view.list.PokemonListAdapter
 import com.wallacemod.android_pokedex.viewmodel.PokemonViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 // POJO -> Plain Old Java Object
 
